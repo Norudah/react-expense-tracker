89b2e654db806efb025d7b825cfc431676a60b11
Norudah <pierucci.romain@gmail.com>
2021-05-26 21:22:33 +0200

    Starting point for versionning

---
+++ src/components/Expenses.js
@@ -0,0 +1,16 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.expenses[0].title}
+        date={props.expenses[0].date}
+        amount={props.expenses[0].amount}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
