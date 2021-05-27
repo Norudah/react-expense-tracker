fd3e319e52d865e907a8aaf48d2c0501cf444b8a
Norudah <pierucci.romain@gmail.com>
2021-05-27 20:06:52 +0200

    Cleaning.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,15 @@
-// React library related
-import { useState } from "react";
-
-// CSS
 import "./ExpenseItem.css";
 
-// Components
 import ExpenseDate from "./ExpenseDate";
 import Card from "../Card/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("Mis à jour");
-    console.log(title);
-  };
-
   return (
-    // <div className="expense-item">
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__desciption">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount} €</div>
-        <button onClick={clickHandler}>COUCOU</button>
       </div>
     </Card>
   );
